b6f9abcad60b7f6b9be8880b42ae8bd6421735b1
joao brito junior <juniorjbn@gmail.com>
2016-07-02 12:25:11 -0300

    Update app.py
    changing msg

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ host = socket.gethostname()
 
 @app.route('/')
 def hello():
-    return '\nHello World!\nIm trying to find a better job.\nSee my Host name is changing by refreshing page %s\n\n' % (host)
+    return '\nHello World!\nIm trying to find a better job.\nSee my Hostname changing by refreshing page if its under an ELB %s\n\n' % (host)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("8000"), debug=True)
